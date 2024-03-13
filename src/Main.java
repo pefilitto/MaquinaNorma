97279bbedff0486037532435bb72392e683abaab
Pedro Filitto <pedrofilitto111@gmail.com>
2024-03-13 11:45:17 -0300

    feat: atribuição e multiplicação

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-
+        MaquinaNorma maquina = new MaquinaNorma();
+        //maquina.adicionaAB(10, 20);
+        //maquina.adicionaABusandoC(10,35,30);
+        //maquina.atribuiABusandoC(20, 40, 50);
+        //maquina.multABusandoCD(1000, 30);
     }
 }
--- src/MaquinaNorma.java
+++ src/MaquinaNorma.java
@@ -1,43 +1,58 @@
 public class MaquinaNorma {
-    public int A;
-    public int B;
-    public int C;
-    public int D;
-    public int flag;
-
-    public void adicionaAB(){
-        flag = 0;
-        while(flag == 0){
+    public void adicionaAB(int A, int B){
+        while(B != 0){
             A = A + 1;
             B = B - 1;
-            if(B == 0){
-                flag = flag + 1;
-            }
         }
+        System.out.println("Soma: " + A);
     }
 
-    public void adicionaABC(){
-        flag = 0;
+    public void adicionaABusandoC(int A, int B, int C){
         C = 0;
-        while(flag == 0){
+        while(B != 0){
             C = C + 1;
             B = B - 1;
-            if(B == 0){
-                flag = flag + 1;
-            }
         }
-
-        flag = 0;
-        while(flag == 0){
+        while(C != 0){
             A = A + 1;
             B = B + 1;
             C = C - 1;
-            if(C == 0){
-                flag = flag + 1;
-            }
         }
+        System.out.println("Soma: " + A);
     }
 
+    public void atribuiABusandoC(int A, int B, int C){
+        C = B;
+        System.out.println("Valor inicial de A: " + A);
+        while(A != 0) A--;
+        while(C != 0){
+            A = A + 1;
+            C = C - 1;
+        }
+        System.out.println("Valor de A depois da atribuição : " + A);
+    }
 
+    public void multABusandoCD(int A, int B){
+        int C = 0, D = 0, E = 0;
 
+        while(B != 0){
+            C++;
+            B--;
+        }
+
+        while(C != 0){
+            B++;
+            C--;
+            while(A != 0){
+                A--;
+                D++;
+                E++;
+            }
+            while(D!=0){
+                D--;
+                A++;
+            }
+        }
+        System.out.println("Multiplicação: " + E);
+    }
 }
